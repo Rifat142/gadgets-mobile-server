e92acf680c78d427464454a0ee235ba18e67ccee
Jh-rifat <jhrifat2017@gmailcom>
2024-06-11 03:34:13 +0600

    fetch data with id

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require('dotenv').config()
-const { MongoClient, ServerApiVersion  } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId  } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -31,9 +31,15 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
 
-    const productCollection = client.db('productdb').collection('product');
+    const productCollection = client.db('newproductdb').collection('newproduct');
 
+    const brandCollection = client.db('bname').collection('bname');
 
+    app.get( '/bname', async(req,res)=>{
+     const cursor = brandCollection.find();
+     const result = await cursor.toArray();
+     res.send(result);
+    })
 
       app.get('/product/:brand', async(req , res)=>{
         
@@ -48,9 +54,33 @@ async function run() {
           products: products
         };
         res.send(result)
+      }),
+
+
+
+    //  all products
+
+      app.get('/products', async(req,res)=>{
+        const cursor = productCollection.find();
+        const result = await cursor.toArray();
+        
+        res.send(result);
+        // console.log(result)
       })
 
-
+    // one product
+    
+    app.get('/products/:id', async(req,res)=>{
+      const id = req.params.id;
+      console.log(id);
+      const query = { _id: new ObjectId(id)}
+      console.log(query);
+      const result = await productCollection.findOne(query);
+      
+      res.send( result);
+      console.log( 'one id result', result);
+      
+    });
       
 
 
